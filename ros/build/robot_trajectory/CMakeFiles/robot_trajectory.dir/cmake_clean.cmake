efdcaceb6fd4b3463f172bb10c8216dbe0422fbf
Quynh Le <manhquynh2018.qng@gmail.com>
2024-04-22 01:40:17 +0700

    fix: Operating time for PID controller
    Edit : target_link_libraries(robot_trajectory ${catkin_LIBRARIES}) in CmakeLists.txt to compile all ros::

--- ros/build/robot_trajectory/CMakeFiles/robot_trajectory.dir/cmake_clean.cmake
+++ ros/build/robot_trajectory/CMakeFiles/robot_trajectory.dir/cmake_clean.cmake
@@ -1,6 +1,7 @@
 file(REMOVE_RECURSE
   "/home/proteenteen/Documents/DO-AN/DO-AN/ros/devel/lib/robot_trajectory/robot_trajectory"
   "/home/proteenteen/Documents/DO-AN/DO-AN/ros/devel/lib/robot_trajectory/robot_trajectory.pdb"
+  "CMakeFiles/robot_trajectory.dir/src/PID_v1.cpp.o"
   "CMakeFiles/robot_trajectory.dir/src/robot_trajectory.cpp.o"
 )
 
