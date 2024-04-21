e073885cb54961173162265d2358b9c75ea129db
Quynh Le <manhquynh2018.qng@gmail.com>
2024-04-21 11:53:21 +0700

    update: Create workspace

---
+++ ros/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/proteenteen/Documents/DO-AN/DO-AN/ros/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/proteenteen/Documents/DO-AN/DO-AN/ros/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/proteenteen/Documents/DO-AN/DO-AN/ros/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/proteenteen/Documents/DO-AN/DO-AN/ros/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/proteenteen/Documents/DO-AN/DO-AN/ros/src:$ROS_PACKAGE_PATH"
