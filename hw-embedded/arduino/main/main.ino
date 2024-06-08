52a676962164a0b4ec930106b3e874e9d60e7757
Quynh Le <manhquynh2018.qng@gmail.com>
2024-06-08 12:47:29 +0700

    test: pos manipulator

--- hw-embedded/arduino/main/main.ino
+++ hw-embedded/arduino/main/main.ino
@@ -25,12 +25,16 @@
 /* Private macros ----------------------------------------------------- */
 /* Private function prototypes ---------------------------------------- */
 static void MobileSpeedCommandCallback(const geometry_msgs::Twist &cmdSpeedMsg);
+static void ManipulatorCommandCallback(const geometry_msgs::Point &cmdPos);
 
 /* Public variables --------------------------------------------------- */
 /* Private variables -------------------------------------------------- */
+float x, y, z;
+
 // ROS config
 ros::NodeHandle nodeHandle;
 ros::Subscriber<geometry_msgs::Twist> subMobileSpeedCmd("cmd_vel", MobileSpeedCommandCallback);
+ros::Subscriber<geometry_msgs::Point> subManiPos("arm_position", ManipulatorCommandCallback);
 
 geometry_msgs::Twist MobileSpeedMsg;
 ros::Publisher pubMobileSpeed("pub_mobile_speed", &MobileSpeedMsg);
@@ -49,6 +53,7 @@ void setup()
   nodeHandle.initNode();
   nodeHandle.getHardware()->setBaud(57600);
   nodeHandle.subscribe(subMobileSpeedCmd);
+  nodeHandle.subscribe(subManiPos);
   nodeHandle.advertise(pubMobileSpeed);
   nodeHandle.advertise(pubMobilePosMsg);
 
@@ -63,13 +68,13 @@ void setup()
 void loop()
 {
 
-  Serial2.print(g_MobileSpeedCommand.x_vel + String(", "));
-  Serial2.print(g_MobileSpeedCommand.y_vel + String(", "));
-  Serial2.print(g_MobileSpeedCommand.theta_vel + String(", "));
+  Serial2.print(String("Mobile: ") + g_MobileSpeedCommand.x_vel + String(", ") + g_MobileSpeedCommand.y_vel + String(", ") + g_MobileSpeedCommand.theta_vel + String(", "));
   Serial2.println("");
 
+  Serial2.print(String("Manipulator: ") + x + String(", ") + y + String(", ") + z + String("\n"));
+
   nodeHandle.spinOnce();
-  delay(10);
+  delay(100);
 }
 
 static void MobileSpeedCommandCallback(const geometry_msgs::Twist &cmdSpeedMsg)
@@ -77,5 +82,12 @@ static void MobileSpeedCommandCallback(const geometry_msgs::Twist &cmdSpeedMsg)
   g_MobileSpeedCommand.x_vel     = cmdSpeedMsg.linear.x;
   g_MobileSpeedCommand.y_vel     = cmdSpeedMsg.linear.y;
   g_MobileSpeedCommand.theta_vel = cmdSpeedMsg.angular.z;
+}
+
+static void ManipulatorCommandCallback(const geometry_msgs::Point &cmdPos)
+{
+  x = cmdPos.x;
+  y = cmdPos.y;
+  z =cmdPos.z;
 }
 /* End of file -------------------------------------------------------- */
